999d2028e5f0330a74baa3a5c6ddadd59d3ad343
Shubham Kumar <shubhamkumarpro1@gmail.om>
2021-06-02 16:12:25 +0530

    created the package folder

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/skpro19/catkin_ws/src/my_global_planner
+# Build directory: /home/skpro19/catkin_ws/src/my_global_planner/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/skpro19/catkin_ws/src/my_global_planner/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/skpro19/catkin_ws/src/my_global_planner/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/skpro19/catkin_ws/src/my_global_planner/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/skpro19/catkin_ws/src/my_global_planner:$ROS_PACKAGE_PATH"
