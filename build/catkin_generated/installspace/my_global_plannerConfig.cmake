56586e8e666ac5bef325dc66ad50c1adfafe383f
Shubham Kumar <shubhamkumarpro1@gmail.om>
2021-06-02 16:26:53 +0530

    Updated CMakeList and package.xml

--- build/catkin_generated/installspace/my_global_plannerConfig.cmake
+++ build/catkin_generated/installspace/my_global_plannerConfig.cmake
@@ -185,7 +185,7 @@ foreach(t ${my_global_planner_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "")
+set(depends "nav_msgs;roscpp;std_msgs;message_runtime;move_base_msgs;move_base;visualization_msgs")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
--- build/catkin_generated/ordered_paths.cmake
+++ build/catkin_generated/ordered_paths.cmake
@@ -1,1 +1,1 @@
-set(ORDERED_PATHS "/opt/ros/melodic/lib")
+set(ORDERED_PATHS "/home/skpro19/catkin_ws/devel/.private/actionlib/lib;/home/skpro19/catkin_ws/devel/.private/costmap_2d/lib;/home/skpro19/catkin_ws/devel/.private/voxel_grid/lib;/opt/ros/melodic/lib")
