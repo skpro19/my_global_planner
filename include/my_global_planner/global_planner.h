2f622b0a53cff4b589580a79675d019fb489d5d7
Shubham Kumar <shubhamkumarpro1@gmail.om>
2021-06-09 15:19:07 +0530

    Minor fixes

--- include/my_global_planner/global_planner.h
+++ include/my_global_planner/global_planner.h
@@ -1,34 +1,41 @@
- /** include the libraries you need in your planner here */
- /** for global path planner interface */
- #include <ros/ros.h>
- #include <costmap_2d/costmap_2d_ros.h>
- #include <costmap_2d/costmap_2d.h>
- #include <nav_core/base_global_planner.h>
- #include <geometry_msgs/PoseStamped.h>
- #include <angles/angles.h>
- #include <base_local_planner/world_model.h>
- #include <base_local_planner/costmap_model.h>
+/** include the libraries you need in your planner here */
+/** for global path planner interface */
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
 
- using std::string;
+using std::string;
 
- #ifndef GLOBAL_PLANNER_CPP
- #define GLOBAL_PLANNER_CPP
+#ifndef GLOBAL_PLANNER_CPP
+#define GLOBAL_PLANNER_CPP
 
- namespace global_planner {
+namespace global_planner {
 
- class GlobalPlanner : public nav_core::BaseGlobalPlanner {
- public:
+    class GlobalPlanner : public nav_core::BaseGlobalPlanner {
 
-  GlobalPlanner();
-  GlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+        public:
 
-  /** overridden classes from interface nav_core::BaseGlobalPlanner **/
-  void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
-  bool makePlan(const geometry_msgs::PoseStamped& start,
-                const geometry_msgs::PoseStamped& goal,
-                std::vector<geometry_msgs::PoseStamped>& plan
-               );
-  };
- };
- 
- #endif
+            GlobalPlanner();
+            GlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+            void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+            bool makePlan(const geometry_msgs::PoseStamped& start,
+                            const geometry_msgs::PoseStamped& goal,
+                            std::vector<geometry_msgs::PoseStamped>& plan
+                        );
+
+        private: 
+
+            costmap_2d::Costmap2D* costmap_ros_;
+            costmap_2d::Costmap2DROS *costmap_ros;
+    };
+
+};
+
+#endif
--- src/global_planner.cpp
+++ src/global_planner.cpp
@@ -7,7 +7,6 @@
 
  using namespace std;
 
- //Default Constructor
  namespace global_planner {
 
  GlobalPlanner::GlobalPlanner (){
@@ -21,26 +20,63 @@
 
  void GlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
 
+    costmap_ros_ = costmap_ros->getCostmap(); 
+
+    cout << "global_frame: " << costmap_ros->getGlobalFrameID() << endl;
+
  }
 
  bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,  std::vector<geometry_msgs::PoseStamped>& plan ){
 
-    plan.push_back(start);
-   for (int i=0; i<20; i++){
-     geometry_msgs::PoseStamped new_goal = goal;
-     tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
 
-      new_goal.pose.position.x = -2.5+(0.05*i);
-      new_goal.pose.position.y = -3.5+(0.05*i);
+   string frame_id = start.header.frame_id; 
 
-      new_goal.pose.orientation.x = goal_quat.x();
-      new_goal.pose.orientation.y = goal_quat.y();
-      new_goal.pose.orientation.z = goal_quat.z();
-      new_goal.pose.orientation.w = goal_quat.w();
+   cout << "frame_id: " << frame_id << endl; 
 
-   plan.push_back(new_goal);
+   if(frame_id != "map") {
+
+     cout << "Start Pose is not in map frame! ---- ABORTING" << endl;
+     return false;
+
    }
-   plan.push_back(goal);
-  return true;
+
+    double wx_i = start.pose.position.x, wy_i = start.pose.position.y;
+    double wx_f = goal.pose.position.x, wy_f = goal.pose.position.y;
+
+
+    __uint32_t mx_i, my_i, mx_f, my_f;
+
+    costmap_ros_->worldToMap(wx_i, wy_i, mx_i, my_i);
+
+    costmap_ros_->worldToMap(wx_f, wy_f, mx_f, my_f);
+
+    __uint32_t mx_c = mx_i, my_c = my_i;
+
+    plan.push_back(start);
+    
+    while(my_c  < my_f) {
+
+      my_c++;
+
+      double wx_c, wy_c;
+
+      costmap_ros_->mapToWorld(mx_c, my_c, wx_c, wy_c);
+
+      geometry_msgs::PoseStamped next_pose = start;
+
+      next_pose.pose.position.x = wx_c; 
+      next_pose.pose.position.y = wy_c;
+      
+      plan.push_back(next_pose);
+
+    } 
+
+    cout << "Size of plan: " << (int)plan.size() << endl;
+
+    return true;
+
+  
  }
+
+
  };
